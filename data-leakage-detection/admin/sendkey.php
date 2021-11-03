e5ddeb8448883c3278a7b897571e42024ad3e4a3
Mounvi <mounvip1525@gmail.com>
2021-11-04 01:38:27 +0530

    revamp:send key and msg and upload changes

--- data-leakage-detection/admin/sendkey.php
+++ data-leakage-detection/admin/sendkey.php
@@ -3,14 +3,13 @@
 <head>
 	<title>Data Leakage Detection</title>
 	<meta charset="utf-8" />
-	
 	<link rel="stylesheet" href="stylesheet.css" type="text/css" />
+	<link rel="stylesheet" href="../stylesheet.css" type="text/css" />
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 
 </head>
 <?php
 session_start(); //Start the session
-
 if (!isset($_SESSION['name'])) {
         echo "Please Login";
 		//$_SESSION['error'] = "Please Login First";
@@ -18,193 +17,58 @@ if (!isset($_SESSION['name'])) {
 		} if (!$_SESSION['name']){
 		      echo  header("Location: http://localhost/data lekage detaction/adminlogin.php");
 		}
-
-		
 		else{
-		
 		define('ADMIN',$_SESSION['name']); //Get the user name from the previously registered super global variable
 		//if(!session_is_registered("admin")){ //If session not registered
-//header("location:login.php"); // Redirect to login.php page
-//}
-//else //Continue to current page
-header( 'Content-Type: text/html; charset=utf-8' );
-//include'includes/conn.php';
- }
- 
+		//header("location:login.php"); // Redirect to login.php page
+		//}
+		//else //Continue to current page
+		header( 'Content-Type: text/html; charset=utf-8' );
+		//include'includes/conn.php';
+    }
 ?>
-<body class="body">
-	
-	<header class="mainHeader">
-		<nav><ul>
-			<li ><a href="admin.php">Home</a></li>
-			<li><a href="upload.php">Upload Article</a></li>
-			<li><a href="view file.php">View File</a></li>
-			<li ><a href="leakfile.php">LeakFile</a></li>
-			<li class="active"><a href="sendkey.php">SendKey</a></li>
-			
-			
-		</ul></nav>
-	</header>
-		
-	<div class="mainContent1">
-		<div class="content">	
-				<article class="topcontent1">	
-					<header>
-						<h2><a href="#" rel="bookmark" title="Permalink to this POST TITLE">Send Key</a></h2>
-					</header>
-					
-					<footer>
-					</footer>
-					
-					<content>
-						<p >
-						
-						<h2><a href="#" rel="bookmark" title="Permalink to this POST TITLE">UserRequest:</a></h2>
-						<table align="center" cellpadding="9" cellspacing="2" width="10" >
-							<tr bgcolor="green"><td>id</td><td>UserName</td><td>Filename</td></tr>
-		
-						<?PHP
-				
-				$con = mysqli_connect("localhost","root","Mounvi@123");
-                                if (!$con)
-                                    echo('Could not connect: ' . mysqli_error());
-                                else
-                                {
-                                    mysqli_select_db($con, "dataleakage");
-									
-
-
-$qry="Select * FROM askkey where reciver='admin' and status='no'";
-	$result=mysqli_query($con, $qry);
-while($w1=mysqli_fetch_array($result))
-{
-	echo'			<tr bgcolor="white">
-	
-	<td>'.$w1["id"].'    </td><td>     '.$w1["user"].'	
-					</td><td>'.$w1["filename"].'
-
-	</td>
-</tr>
-
-
-	
-	';
-		
-}
-								}
-
-?>
-							
-							
-
-						</p>
-						
-</table>
-
-						<article class="topcontent2">	
-				
-						
-						
-							<p>		
-
-<h2><a href="#" rel="bookmark" title="Permalink to this POST TITLE">View File And Key:</a></h2>		
-<table align="center" cellpadding="9" cellspacing="1" width="10" >
-							<tr bgcolor="green"><td>FileName</td><td>key</td></tr>
-							
-					<?PHP
-				
-				$con = mysqli_connect("localhost","root","Mounvi@123");
-                                if (!$con)
-                                    echo('Could not connect: ' . mysqli_error());
-                                else
-                                {
-                                    mysqli_select_db($con, "dataleakage");
-									
-
-
-$qry="Select * FROM presentation";
-	$result=mysqli_query($con, $qry);
-while($w1=mysqli_fetch_array($result))
-{
-	echo'			<tr bgcolor="white">
-	
-	<td>'.$w1["subject"].'    </td><td>     '.$w1["Topic"].'	
-					</td>
-</tr>
-
-
-	
-	';
-		
-}
-								}
-
-?>		
-							
-							
-				</p>
-				
-</table>
-
-						</content>
-					
-				</article>
-
-			</div>
-<aside class="top-sidebar">
-					<article>
-					<h2>Welcome: <?php echo $_SESSION['name']/*Echo the username */ ?></h2>
-					<li><a href="logout.php">Logout</a></li>
-					
-					<p ><h5 >Send Key:</h5></br>
-							<form name="s" action="sendkeytouser.php" method="POST" onsubmit="return valid()">
-            <table  bgcolor="brown" align="center" cellpadding="" cellspacing="" width="">
-              <tr> 
-                <td colspan="2" align="center"><font size="2"><b>
-                  </b></font></td>
-              </tr>
-                <tr> 
-                <td><strong>Send2:</strong></td>
-                <td>&nbsp;&nbsp;&nbsp;&nbsp; <input type="text" name="a1" id="a1" class="b"></td>
-              </tr>
-			   <tr> 
-                <td><strong>
-				FileName:</strong></td>
-                <td>&nbsp;&nbsp;&nbsp;&nbsp; <input type="text" name="a3" id="a3" class="b"></td>
-              </tr>
-              <tr> 
-                <td><strong>Key:</strong></td>
-                <td>&nbsp;&nbsp;&nbsp;&nbsp; <input type="text" name="a2" id="a2" class="b">
-</td>
-              </tr>
-                <td></td>
-                <td><input type="submit" name="s" value="Send" class="b1" > 
-                  &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; 
-                  <input type="reset" name="r" value="clear" class="b1"></td>
-              </tr>
-            </table>
-          </form>
-							
-							
-							
-							
-				</p>
-				    </article>
-				</aside>	
-		</div>
-			
-		
-		
-				    
-				
-		</div>
-			
-				
-	</div>
-	
-	<footer class="mainFooter">
-		<p>CSE3501 Project</p>
-	</footer>
-
+<body style="overflow:hidden">
+	<nav>
+        <h3><a href="admin.php">HOME</a></h3>
+		<h3><a href="m_user.php">USERS</a></h3>
+		<h3><a href="m_arti.php">ARTICLES</a></h3>
+        <h3><a href="upload.php">UPLOAD</a></h3>
+		<h3><a href="viewmsg.php">MESSAGES</a></h3>
+		<h3><a href="leakfile.php">LEAK USER</a></h3>
+        <h3><a href="sendkey.php" class="admin-active-nav">KEYS</a></h3>
+        <h3><a href="logout.php">LOGOUT</a></h3>
+    </nav>
+	<div class="req-main">
+        <form name="s" action="sendkeytouser.php" method="POST" onsubmit="return valid()" class="auth-form">
+            <h1>SEND KEYS</h1>
+            <label for="name">Send to User</label>
+            <input type="text" name="a1" id="a1">
+            <label for="file">File Name</label>
+            <input type="text" name="a3" id="a3">
+            <label for="key">Key</label>
+            <input type="text" name="a2" id="a2">
+			<input type="submit" name="s" value="Share Key" class="c-btn c-btn-2" style="width:50%!important"> 
+		</form>
+        <div>
+            <ul class="admin-requests">
+			<?PHP
+			$con = mysqli_connect("localhost","root","Mounvi@123");
+				if (!$con)
+					echo('Could not connect: ' . mysqli_error());
+				else
+				{
+					mysqli_select_db($con, "dataleakage");
+					$qry="Select * FROM askkey where reciver='admin' and status='no'";
+						$result=mysqli_query($con, $qry);
+					while($w1=mysqli_fetch_array($result))
+					{
+						echo
+						'<li>'.$w1["id"].' requests for <b>'.$w1["filename"].'</b></li>';
+					}
+				}
+			?>
+            </ul>
+        </div>
+    </div>
 </body>
 </html>
--- data-leakage-detection/admin/sendmsg.php
+++ data-leakage-detection/admin/sendmsg.php
@@ -8,107 +8,49 @@ if (!isset($_SESSION['name'])) {
 		} if (!$_SESSION['name']){
 		      echo  header("Location: http://localhost/data lekage detaction/adminlogin.php");
 		}
-
-		
 		else{
-		
 		define('ADMIN',$_SESSION['name']); //Get the user name from the previously registered super global variable
 		//if(!session_is_registered("admin")){ //If session not registered
-//header("location:login.php"); // Redirect to login.php page
-//}
-//else //Continue to current page
-header( 'Content-Type: text/html; charset=utf-8' );
-//include'includes/conn.php';
- }
- 
+		//header("location:login.php"); // Redirect to login.php page
+		//}
+		//else //Continue to current page
+		header( 'Content-Type: text/html; charset=utf-8' );
+		//include'includes/conn.php';
+ 	}
 ?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
 	<title>Data Leakage Detection</title>
 	<meta charset="utf-8" />
-	
 	<link rel="stylesheet" href="stylesheet.css" type="text/css" />
+	<link rel="stylesheet" href="../stylesheet.css" type="text/css" />
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
-
 </head>
 
-<body class="body">
-	
-	<header class="mainHeader">
-		<nav><ul>
-			
-			<li class="active"><a href="admin.php">Home</a></li>
-			<li><a href="upload.php">Publish Article</a></li>
-			<li><a href="view file.php">View File</a></li>
-			<li ><a href="leakfile.php">LeakFile</a></li>
-			<li ><a href="sendkey.php">SendKey</a></li>
-	</ul></nav>
-	</header>
-		
-	<div class="mainContent1">
-		<div class="content">	
-				<article class="topcontent1">	
-					<header>
-						<h2><a href="#" rel="bookmark" title="Permalink to this POST TITLE"> Send Message</a></h2>
-					</header>
-					
-					
-					<content>
-						<p>	  <form name="s" action="msg.php" method="POST" onsubmit="return valid()">
-            <table align="center" cellpadding="" cellspacing="" width="">
-              <tr> 
-                <td colspan="2" align="center"><font size="2"><b>
-                  </b></font></td>
-              </tr>
-              <tr> 
-                <td><font face="Courier New" size="+1"><strong>User EmailId</strong></font></td>
-                <td>&nbsp;&nbsp;&nbsp;&nbsp; <input type="text" name="a1" id="a1" ></td>
-              </tr>
-              <tr> 
-                <td><font face="Courier New" size="+1"><strong>User Name</strong></font></td>
-                <td>&nbsp;&nbsp;&nbsp;&nbsp; <input type="text" name="a2" id="a2" ></td>
-              </tr>
-              <tr> 
-                <td><font face="Courier New" size="+1"><strong>Message</strong></font></td>
-                <td>&nbsp;&nbsp;&nbsp;&nbsp; <textarea name="a3" rows="5" id="a3" cols="40"></textarea>
-</td>
-              </tr>
-                <td></td>
-                <td><input type="submit" name="s" value="Send"  > 
-                  &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; 
-                  <input type="reset" name="r" value="clear" class="b1"></td>
-              </tr>
-            </table>
-          </form>
-        </div>
-   
+<body>
+	<nav>
+        <h3><a href="admin.php">HOME</a></h3>
+		<h3><a href="m_user.php">USERS</a></h3>
+		<h3><a href="m_arti.php">ARTICLES</a></h3>
+        <h3><a href="upload.php">UPLOAD</a></h3>
+		<h3><a href="viewmsg.php" class="admin-active-nav">MESSAGES</a></h3>
+		<h3><a href="leakfile.php">LEAK USER</a></h3>
+        <h3><a href="sendkey.php">KEYS</a></h3>
+        <h3><a href="logout.php">LOGOUT</a></h3>
+    </nav>
 
-
-
-
-						</p>
-	</content>
-					
-				</article>
-
-				</div>
-<aside class="top-sidebar">
-					<article>
-					<h2>Welcome: <?php echo $_SESSION['name']/*Echo the username */ ?></h2>
-					<li><a href="logout.php">Logout</a></li>
-					
-					<p></p>
-				    </article>
-				</aside>	
-		</div>
-			
-				
-	</div>
-	
-	<footer class="mainFooter">
-		<p>CSE3501 Project</p>
-	</footer>
-
+	<div style="width:40%;margin:auto">
+        <img src="../img/message.png" alt="message" class="msg-img"/>
+        <form name="s" action="msg.php" method="POST" onsubmit="return valid()" class="a-msg auth-form">
+            <label for="name">User Name</label>
+            <input type="text" name="a2" id="a2" >
+            <label for="email">User Email</label>
+            <input type="text" name="a1" id="a1" >
+            <label for="msg">Send Message</label>
+            <textarea name="a3" rows="5" id="a3" cols="40"></textarea>
+			<button type="submit" name="s" class="c-btn">SEND </button>
+		</form>
+    </div>
 </body>
 </html>
